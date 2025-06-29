aca660c173da3bc4d2ba76b7e3c89d98bfcbdf8c
Rav01S <ravil05safydinov@gmail.com>
2025-06-29 22:54:06 +0500

    init

--- src/app/(pages)/search/page.tsx
+++ src/app/(pages)/search/page.tsx
@@ -1,12 +1,15 @@
 "use server";
 
 import SearchSubjects from "@/features/search/ui/searchSubjects";
+import { Suspense } from "react";
 
 export default async function Page() {
   return (
     <>
       <h1 className="text-3xl font-bold mb-4">Результаты поиска</h1>
-      <SearchSubjects />
+      <Suspense>
+        <SearchSubjects />
+      </Suspense>
     </>
   );
 }
