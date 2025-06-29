23cee122cbd9a602fa0f3e11de3d7f20afe5a55b
Rav01S <ravil05safydinov@gmail.com>
2025-06-29 22:50:06 +0500

    init

--- src/app/(pages)/(authed)/my-subjects/[id]/page.tsx
+++ src/app/(pages)/(authed)/my-subjects/[id]/page.tsx
@@ -3,8 +3,8 @@
 import EditSubjectForm from "@/features/my-subjects/ui/[id]/EditSubjectForm";
 import { getSubjectById } from "../actions";
 
-export default async function Page(props: { params: { id: string } }) {
-  const subject = await getSubjectById(Number(props.params.id));
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const subject = await getSubjectById(Number((await props.params).id));
   if (!subject) {
     return <div>Предмет не найден</div>;
   }
